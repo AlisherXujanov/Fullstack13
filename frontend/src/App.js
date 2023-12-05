fa7e5a3d356b24243050907004703198cd1d5118
Alisher <alisherxujanov163@gmail.com>
2023-12-05 17:50:33 +0500

    first test component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
+import Test from './components/Test';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+
+      <Test>
+        <h2>This is child tag</h2>
+      </Test>
     </div>
   );
 }
---
+++ frontend/src/components/Test.jsx
@@ -0,0 +1,11 @@
+function Test(props) {
+    return (
+        <div>
+            {props.children}
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia quam debitis autem maxime nesciunt! Beatae, quibusdam tenetur. Odio maiores autem eveniet corporis, facilis consequatur perferendis et officia quos magnam. Nulla!</p>
+        </div>
+    )
+}
+
+
+export default Test;
