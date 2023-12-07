08db96faae95b60107300c228ac89d82cfcf4c63
Alisher <alisherxujanov163@gmail.com>
2023-12-07 17:44:08 +0500

    Nav

---
+++ frontend/src/components/Navigation/index.jsx
@@ -0,0 +1,11 @@
+function Navigation() {
+    return (
+        <div>
+            <nav id="navigation-wrapper">
+                
+            </nav>
+        </div>
+    );
+}
+
+export default Navigation;
