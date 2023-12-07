0849f2f842cdb4af3753abe0c33e6e4f5deacacb
Alisher <alisherxujanov163@gmail.com>
2023-12-07 17:56:50 +0500

    navigation style started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,11 @@
-import Test from './components/Test';
-
-// 1. npm init react-app bemiyya
-// 2. cd bemiyya
-// 3. npm start
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Navigation />
 
-      <Test>
-        <h2>This is child tag</h2>
-      </Test>
+      
     </div>
   );
 }
--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -1,8 +1,23 @@
+import "./style.scss"
+
 function Navigation() {
     return (
         <div>
             <nav id="navigation-wrapper">
-                
+                <div className="left">
+                    <h1>Fonte</h1>
+                </div>
+                <div className="middle">
+                    <a href="/#about">О нас</a>
+                    <a href="/#team">Команда</a>
+                    <a href="/#blog">Блог</a>
+                    <a href="/#products">Продукты</a>
+                    <a href="/#contacts">Контакты</a>
+                </div>
+                <div className="right">
+                    <button>Войти</button>
+                    <a href="/#ru">Рус</a>
+                </div>
             </nav>
         </div>
     );
