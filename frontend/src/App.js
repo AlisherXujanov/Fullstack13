bdb037c7f75e8331ccc6b43d4798041db76f17e9
Alisher <alisherxujanov163@gmail.com>
2023-12-07 16:48:21 +0500

    useState

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import Test from './components/Test';
 
+// 1. npm init react-app bemiyya
+// 2. cd bemiyya
+// 3. npm start
+
 function App() {
   return (
     <div className="App">
--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react"
+
 function Test(props) {
+    const [theme, setTheme] = useState(true)
+
+    const themeColor = {
+        backgroundColor: theme ? "black" : "#fff",
+        color: theme ? "#fff" : "black",
+    }    
+
     return (
-        <div>
-            {props.children}
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia quam debitis autem maxime nesciunt! Beatae, quibusdam tenetur. Odio maiores autem eveniet corporis, facilis consequatur perferendis et officia quos magnam. Nulla!</p>
+        <div style={themeColor}>
+
+            <div>
+                <h1>Hello world</h1>
+                {props.children}
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia quam debitis autem maxime nesciunt! Beatae, quibusdam tenetur. Odio maiores autem eveniet corporis, facilis consequatur perferendis et officia quos magnam. Nulla!</p>
+            </div>
+
+            <hr />
+            <br />
+            <br />
+
+            <button onClick={() => { setTheme(!theme) }}>
+                Change theme
+            </button>
+
+            <hr />
+            <br />
+            <br />
+
+
         </div>
     )
 }
