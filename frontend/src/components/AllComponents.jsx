960ffcc876d3fecdcfd70ec80a042cd7f4c6bab1
Alisher <alisherxujanov163@gmail.com>
2023-12-14 16:28:17 +0500

    routes

---
+++ frontend/src/components/AllComponents.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom' 
+
+function AllComponents() {
+    return (
+        <div className="all-components-wrapper">
+
+        </div>
+    );
+}
+
+export default AllComponents;
