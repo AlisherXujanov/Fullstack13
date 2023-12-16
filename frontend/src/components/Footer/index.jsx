845be768547008cd9dcc3b9b0a8429ce3683a8a4
Alisher <alisherxujanov163@gmail.com>
2023-12-16 17:45:18 +0500

    footer data

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,5 +1,7 @@
 import "./style.scss"
 import Heading from "../common/Heading"
+import { Link } from "react-router-dom"
+import SM from "../../assets/icons/sm.png"
 
 function Footer() {
     return (
@@ -10,8 +12,22 @@ function Footer() {
                         Fonte
                     </Heading>
                 </div>
-                <div className="footer-links"></div>
-                <div className="footer-sm"></div>
+                <div className="footer-links">
+                    <div className="left">
+                        <Link to={"#"}>О нас</Link>
+                        <Link to={"#"}>Команда</Link>
+                        <Link to={"#"}>Блог</Link>
+                        <Link to={"#"}>Продукты</Link>
+                        <Link to={"#"}>Контакты</Link>
+                    </div>
+                    <div className="right">
+                        <Link to={"#"}>Terms and conditions</Link>
+                        <Link to={"#"}>Privacy policy</Link>
+                    </div>
+                </div>
+                <div className="footer-sm">
+                    <img src={SM} alt="Social Media" />
+                </div>
             </div>
             <div className="footer-bottom"></div>
         </div>
