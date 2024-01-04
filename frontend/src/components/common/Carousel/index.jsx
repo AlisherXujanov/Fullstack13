b895082d22895c7cb223ddfdd7a20bdf879740ba
Alisher <alisherxujanov163@gmail.com>
2024-01-04 16:30:34 +0500

    carousel-c-children

--- frontend/src/components/common/Carousel/index.jsx
+++ frontend/src/components/common/Carousel/index.jsx
@@ -2,11 +2,19 @@ import "./style.scss"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
-// <CarouselComponent  images={[..., ..., ...]} />
+// <CarouselComponent  images={[..., ..., ...]}  blurred={true} />
 
 function CarouselComponent(props) {
+
+    const imgStyle = {
+        filter: props.blurred ? "grayscale(100%) brightness(0.5)" : "none"
+    }
+
     return (
         <div className="carousel-c-wrapper">
+            <div className="carousel-c-children">
+                {props.children}
+            </div>
             <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false}>
                 {
                     props.images && props.images.map((img, index) => {
@@ -17,6 +25,7 @@ function CarouselComponent(props) {
                                     alt={"Carousel-img-" + index} 
                                     width={"100%"}
                                     height={600}
+                                    style={imgStyle}
                                 />
                             </div>
                         )
