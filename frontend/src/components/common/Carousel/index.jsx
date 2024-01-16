d2aa0091472b97e33c72ae3973bd3fb6eb0e2848
Alisher <alisherxujanov163@gmail.com>
2024-01-16 17:24:58 +0500

    installed carousel

--- frontend/src/components/common/Carousel/index.jsx
+++ frontend/src/components/common/Carousel/index.jsx
@@ -1,7 +1,26 @@
-function Carousel() {
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import './style.scss'
+
+function CarouselComponent(props) {
+    // <CarouselComponent images={...} />
     return (
-        <></>
+        <Carousel>
+            {
+                props.images?.map((image, index) => {
+                    return (
+                        <div key={index}>
+                            <img 
+                                src={image} alt="Slide" 
+                                height={600} 
+                                width={"100%"} 
+                            />
+                        </div>
+                    )
+                })
+            }
+        </Carousel>
     );
 }
 
-export default Carousel;
+export default CarouselComponent;
