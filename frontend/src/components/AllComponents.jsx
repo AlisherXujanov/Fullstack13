84e50a760020fd11a42e638cfa3eec99601e746e
Alisher <alisherxujanov163@gmail.com>
2024-01-16 16:55:22 +0500

    Created heading

--- frontend/src/components/AllComponents.jsx
+++ frontend/src/components/AllComponents.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Navigation from "./Navigation"
 import About from "./About"
 import NoPage from "./NoPage"
+import Products from "./Products"
 
 function AllComponents() {
     const location = useLocation();
@@ -14,6 +15,7 @@ function AllComponents() {
                 <Route path="/" element={<Navigation />} >
                     <Route index element={<About />} />
                     <Route path="/about" element={<About />} />
+                    <Route path="/products" element={<Products />} />
                     <Route path="*" element={<NoPage />} />
                 </Route>
             </Routes>
---
+++ frontend/src/components/common/Carousel/index.jsx
@@ -0,0 +1,7 @@
+function Carousel() {
+    return (
+        <></>
+    );
+}
+
+export default Carousel;
