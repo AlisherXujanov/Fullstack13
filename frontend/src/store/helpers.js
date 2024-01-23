0ea6bee474031a48c8eb677578b1f4035dece16c
Alisher <alisherxujanov163@gmail.com>
2024-01-23 17:11:50 +0500

    useReducer

--- frontend/src/store/helpers.js
+++ frontend/src/store/helpers.js
@@ -1,5 +1,12 @@
 function globalReducer(state, payload) {
-    
+    switch (payload.type) {
+        case "text":
+            return { ...state, text: payload.value }
+        case "range":
+            return { ...state, range: payload.value }
+        default:
+            return state
+    }
 }
 
 
