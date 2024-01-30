e6fb24b48d703a8409f0cfc70a4babf1ae59b801
Alisher <alisherxujanov163@gmail.com>
2024-01-30 17:05:53 +0500

    usage of context (useContext, createContext)

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -1,11 +1,58 @@
 import './style.scss'
 
+// Learning useContext
 
-function Products(props) {
+import { useContext, createContext, useState } from 'react'
+const context = createContext()
+
+
+function C1() {
+    return (
+        <>
+            <h2>Component 1</h2>
+            <C2 />
+            <hr />
+        </>
+    )
+}
+function C2() {
+    return (
+        <>
+            <h2>Component 2</h2>
+            <C3 />
+            <hr />
+        </>
+    )
+}
+function C3() {
+    const payload = useContext(context)
+
+    return (
+        <>
+            <h2>Component 3</h2>
+            <input type="text"
+                onChange={(e) => { payload.setInfo(e.target.value) }}
+            />
+        </>
+    )
+}
+
+
+function Products() {
+    const [info, setInfo] = useState("Hello world")
+
+    const payload = { info:info,  setInfo:setInfo }
+
     return (
         <div id="products-wrapper">
-            <h2>Products</h2>
-            
+
+            <context.Provider value={payload}>
+                <h2>Products</h2>
+                <p>{info}</p>
+
+                <C1 />
+            </context.Provider>
+
         </div>
     );
 }
