db51e1d09d800e0c1d8eda7144684bef7b7fac2d
Alisher <alisherxujanov163@gmail.com>
2024-02-01 17:20:59 +0500

    axios

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -7,23 +7,23 @@ const URL = "https://reqres.in/api/users?page=2"
 
 function Products(props) {
     const { store, setStore } = useContext(context)
-    const [users, setUsers] = useState([])
+    // const [users, setUsers] = useState([])
 
-    useEffect(()=>{
-        fetch(URL)
-            .then(res => res.json())
-            .then(data => setUsers(data.data))
+    useEffect(() => {
+        // fetch(URL)
+        //     .then(res => res.json())
+        //     .then(data => setUsers(data.data))
     }, [])
 
     return (
         <div id="products-wrapper">
             <h2>Products</h2>
-            <h3>{ store.count }</h3>
-            <button className='warning-btn' onClick={(e) => { setStore({type: 'increment'}) }}>Increment</button>
-            
+            <h3>{store.count}</h3>
+            <button className='warning-btn' onClick={(e) => { setStore({ type: 'increment' }) }}>Increment</button>
+
 
             <div className="container">
-                {
+                {/* {
                     users.map((user, index) => {
                         return (
                             <div key={index}>
@@ -34,7 +34,48 @@ function Products(props) {
                             </div>
                         )
                     })
-                }
+                } */}
+                <Get url={URL} params={{}}>
+                    {(error, response, isLoading, makeRequest, axios) => {
+                        if (error) {
+                            return (
+                                <div>
+                                    Something bad happened: {error.message}
+                                    <button onClick={() => makeRequest({ params: { reload: true } })}>
+                                        Retry
+                                    </button>
+                                </div>
+                            )
+                        }
+                        else if (isLoading) {
+                            return (<div>Loading...</div>)
+                        }
+                        else if (response !== null) {
+                            return (
+                                <div>
+                                    <button className='warning-btn' 
+                                        onClick={() => makeRequest({ params: { refresh: true } })}
+                                    >
+                                        Refresh
+                                    </button>
+                                    {
+                                        response.data.data.map((user, index) => {
+                                            return (
+                                                <div key={index}>
+                                                    <h2>{user.first_name}</h2>
+                                                    <h3>Email: {user.email}</h3>
+                                                    <img src={user.avatar} alt="Avatar"
+                                                        width={100} height={100} />
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            )
+                        }
+                        return (<div>Default message before request is made.</div>)
+                    }}
+                </Get>
             </div>
         </div>
     );
