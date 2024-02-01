8c796e5156470c05c2377d77f3c1c7a6c71b9a9a
Alisher <alisherxujanov163@gmail.com>
2024-02-01 16:36:09 +0500

    useEffect

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -1,17 +1,21 @@
 import './style.scss'
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { context } from "../../store"
 
 function Products(props) {
     const { store, setStore } = useContext(context)
 
+    useEffect(()=>{
+        console.log("Hello world")
+    }, [])
+
     return (
         <div id="products-wrapper">
             <h2>Products</h2>
             <h3>{ store.count }</h3>
-            <button className='warning-btn' onClick={(e) => { setStore({type: 'increment'}) }}>
-                Increment
-            </button>
+            <button className='warning-btn' onClick={(e) => { setStore({type: 'increment'}) }}>Increment</button>
+
+
         </div>
     );
 }
