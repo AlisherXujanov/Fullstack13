ef7f4f98a7fa82dcf92ac90d1fe9cbd058f9cbce
Alisher <alisherxujanov163@gmail.com>
2024-02-03 17:26:54 +0500

    blog page logics

--- frontend/src/components/Blog/Details.jsx
+++ frontend/src/components/Blog/Details.jsx
@@ -1,13 +1,18 @@
 import { useParams } from "react-router-dom"
 import "./details.scss"
+import BlogJSON from "../../db/blog.json"
 
 function Details() {
     const { id } = useParams()
 
+    const blog = BlogJSON.find(blog => blog.id === parseInt(id))
+    const subtitle = blog.title.split(" ").slice(0, 3).join(" ")
+
     return (
         <div className="blog-details-wrapper">
-            <h1>Details</h1>
-            {String(id)}
+            <p>Blog / {subtitle}</p>
+
+            <h1>{blog.title}</h1>
         </div>
     );
 }
