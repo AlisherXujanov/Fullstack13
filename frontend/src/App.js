51476cc4222d0a3bc1c7e77caa98cb166bb17fd7
Alisher <alisherxujanov163@gmail.com>
2024-02-06 16:42:42 +0500

    installed and configured i18n

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <ToastContainer />
-      
+
       <BrowserRouter>
         <context.Provider value={contextPayload}>
           <AllComponents />
--- frontend/src/components/Navigation/Nav.jsx
+++ frontend/src/components/Navigation/Nav.jsx
@@ -1,13 +1,24 @@
 import Heading from "../common/Heading"
 import { Link, useNavigate } from 'react-router-dom'
-
+import { context } from "../../store"
+import { useContext } from "react"
+import { useTranslation } from "react-i18next";
 
 // 1. Create a burger
 // 2. Put the input checkbox onto the burger and make it invisible
 // 3. Open the Nav-width div when the checkbox is checked
 
 function Nav(props) {
+    const { store, setStore } = useContext(context);
+    const { t, i18n: { changeLanguage, language } } = useTranslation();
+
     const navigate = useNavigate();
+
+    const handleChangeLanguage = () => {
+        const newLanguage = language === "en" ? "ru" : "en";
+        state.dispatch({ type: "CHANGE_LANG", currentLanguage: newLanguage })
+        changeLanguage(newLanguage);
+    }
 
     const goToTeamsHash = () => {
         navigate('/about');
@@ -18,6 +29,7 @@ function Nav(props) {
             }
         }, 0);
     };
+
 
     return (
         <div className="nav-wrapper">
