b334b531ca1c5c51e3696dea9c96598874b819c3
Alisher <alisherxujanov163@gmail.com>
2024-02-08 17:08:55 +0500

    first test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App.js'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("My first test", () => {
+    render(<App />)
+    const element2 = screen.getByText('Хедж-фонд')
+    expect(element2).toBeInTheDocument()
+})
