f03431c6745487d0e429eb4284d901ea264d5344
Alisher <alisherxujanov163@gmail.com>
2024-06-27 11:26:04 +0500

    created users view

---
+++ backend/app/users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import users
+
+urlpatterns = [
+    path("", users, name='users'),
+]
--- backend/app/users/views.py
+++ backend/app/users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def users(request):
+    return render(request, "users.html")
