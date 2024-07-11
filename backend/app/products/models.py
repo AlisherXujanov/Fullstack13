9a58c3f20467217fed543a46846252f6a7a5781f
Alisher <alisherxujanov163@gmail.com>
2024-07-11 10:37:30 +0500

    small fixes

--- backend/app/products/models.py
+++ backend/app/products/models.py
@@ -3,6 +3,23 @@ from django.contrib.auth.models import User
 import json
 from PIL import Image
 
+class CarImage(models.Model):
+    car = models.ForeignKey('Cars', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='cars/', default='cars/default.png')
+
+    def __str__(self):
+        return f'Image of {self.car.brand} {self.car.model}'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        if img.height > 500 or img.width > 500:
+            output_size = (500, 500)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+
 # Create your models here.
 class Cars(models.Model):
     brand = models.CharField(max_length=50)
@@ -14,8 +31,7 @@ class Cars(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     address = models.CharField(max_length=255)
     likes = models.TextField(default='[]')
-    image = models.ImageField(
-        upload_to='cars/', default='cars/default.png')
+    images = models.ManyToManyField(CarImage)
     category = models.CharField(max_length=50)
     
 
@@ -59,14 +75,7 @@ class Cars(models.Model):
         #     return True
 
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    
 
 
 
