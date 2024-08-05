52c77e76e540a7963068a7805d72e24139233d16
Islom <abdugafarovislom@gmail.com>
2024-08-05 22:21:14 +0500

    add not found page

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
+from nfts import views
+
+handler404 = views.custom_404
 
 urlpatterns = [
     path("", include("nfts.urls")),
--- backend/app/nfts/views.py
+++ backend/app/nfts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,redirect
 from allauth.account.forms import LoginForm, SignupForm
 from django.contrib.auth import logout
+from django.shortcuts import render
 
 def custom_logout(request):
     logout(request)
     return redirect('landing_page')
 
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
 
 def landing_page(request):
     signin_form = LoginForm()
