525e7a5541bf1aba6f9de8b4114086e3b53432aa
azim0nt <kktotov457@gmail.com>
2024-08-05 23:35:14 +0500

    fix bugs

--- backend/app/nfts/views.py
+++ backend/app/nfts/views.py
@@ -1,21 +1,32 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from allauth.account.forms import LoginForm, SignupForm
 from django.contrib.auth import logout
-from django.shortcuts import render
+from django.contrib import messages
+
 
 def custom_logout(request):
     logout(request)
     return redirect('landing_page')
 
+
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
+
 def landing_page(request):
-    signin_form = LoginForm()
-    signup_form = SignupForm()
-    return render(request, 'landing_page.html',{
-        'signin_form':signin_form,
-        'signup_form':signup_form
+    signin_form = LoginForm(request.POST or None)
+    signup_form = SignupForm(request.POST or None)
+
+    if request.method == 'POST':
+        if signin_form.is_valid():
+            return redirect('landing_page')
+        else:
+            messages.error(request, 'Error')
+            return redirect('landing_page')
+
+    return render(request, 'landing_page.html', {
+        'signin_form': signin_form,
+        'signup_form': signup_form
     })
 
 
