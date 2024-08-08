678c21f7617d4f82ad8c313bb15e736873089daa
azim0nt <kktotov457@gmail.com>
2024-08-08 07:28:40 +0500

    create nft list & nft page & dropdown

---
+++ backend/app/app/context_processors.py
@@ -0,0 +1,9 @@
+
+from allauth.account.forms import LoginForm, SignupForm
+
+
+def auth_forms(request):
+    return {
+        'signin_form': LoginForm(),
+        'signup_form': SignupForm(),
+    }
