b1f09b747075052cc094faae3cad129dcbc2c6e2
Alisher <alisherxujanov163@gmail.com>
2024-08-10 10:17:34 +0500

    added favorite icon into details page

--- backend/app/nfts/views.py
+++ backend/app/nfts/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from .models import NFTs
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .usecases import *
-
 
 
 def custom_logout(request):
@@ -20,24 +19,24 @@ def landing_page(request):
     return render(request, 'landing_page.html')
 
 
-def add_to_favorites(request, pk:int):
+def add_to_favorites(request, pk: int):
     if add_to_favorites_fn(request, pk):
         messages.success(request, 'NFT added to favorites')
     else:
         messages.warning(request, 'Already in favorites')
 
-    return redirect('explore')
+    referee = request.META.get('HTTP_REFERER')
+    return redirect(referee)
 
 
-def remove_from_favorites(request, pk:int):
+def remove_from_favorites(request, pk: int):
     if remove_from_favorites_fn(request, pk):
         messages.success(request, 'NFT removed from favorites')
     else:
         messages.warning(request, 'This NFT is not in favorites')
 
-    return redirect('explore')
-
-
+    referee = request.META.get('HTTP_REFERER')
+    return redirect(referee)
 
 
 def create_nft(request):
@@ -49,22 +48,22 @@ def create_nft(request):
         # collection = request.POST['collection']
 
         NFTs.objects.create(
-            name = name,
-            description = description,
-            price = price,
-            image = img,
-            owner = request.user
+            name=name,
+            description=description,
+            price=price,
+            image=img,
+            owner=request.user
         )
         messages.success(request, 'NFT created successfully')
         return redirect('explore')
-    
+
     context = {
         'length_of_nfts': NFTs.objects.all().count()+1
     }
     return render(request, 'create_nft.html', context)
 
 
-def update_nft(request, pk:int):
+def update_nft(request, pk: int):
     nft = NFTs.objects.get(id=pk)
 
     if request.method == "POST":
@@ -72,7 +71,7 @@ def update_nft(request, pk:int):
         description = request.POST['description'].strip()
         price = request.POST['price']
         img = request.FILES['image']
-        
+
         nft.name = name
         nft.description = description
         nft.price = price
@@ -81,26 +80,30 @@ def update_nft(request, pk:int):
         messages.success(request, 'NFT updated successfully')
 
         # Go to details page
-        return redirect('nft_details', pk=pk) 
+        return redirect('nft_details', pk=pk)
 
-    context = {'nft_obj':nft}
+    context = {'nft_obj': nft}
     return render(request, 'update_nft.html', context)
 
-    
+
 def explore(request):
     context = {
-        'nfts':NFTs.objects.all(),
+        'nfts': NFTs.objects.all(),
         'favorites': request.session.get('favorites', [])
     }
-    return render(request,'explore.html', context)
+    return render(request, 'explore.html', context)
 
 
-def nft_details(request, pk:int):
+def nft_details(request, pk: int):
     nft = NFTs.objects.get(id=pk)
-    return render(request, 'nft_details.html', {'nft':nft})
+    context = {
+        'nft': nft,
+        'favorites': request.session.get('favorites', [])
+    }
+    return render(request, 'nft_details.html', context)
 
 
-def delete_nft(request, pk:int):
+def delete_nft(request, pk: int):
     nft = NFTs.objects.get(id=pk)
     nft.delete()
     messages.success(request, 'NFT deleted successfully')
