24c01118210bbcda4b0c16361136a1f0b5558e3a
Alisher <alisherxujanov163@gmail.com>
2024-08-15 10:59:01 +0500

    chat messages

---
+++ backend/app/users/usecases.py
@@ -0,0 +1,30 @@
+from .models import Messages
+from django.contrib.auth.models import User
+
+
+def get_ordered_messages(user: User, companion: User) -> list:
+    owner_messages = list(Messages.objects.filter(
+        owner=user, sender=companion).order_by('created_at'))
+    sender_messages = list(Messages.objects.filter(
+        owner=companion, sender=user).order_by('created_at'))
+    target_chat_messages = list(owner_messages + sender_messages)
+
+    filtered_by_day_month_year = [
+        {
+            "day": message.created_at.day,
+            "month": message.created_at.month,
+            "year": message.created_at.year,
+            "message": {
+                "time": message.created_at.strftime("%H:%M"),
+                "content": message.content,
+                "sender": "companion" if message.sender == companion else "myself"
+            }
+        }
+        for message in target_chat_messages
+    ]
+    print(filtered_by_day_month_year)
+    return filtered_by_day_month_year
+
+
+def get_chat_messages(user: User, companion: User) -> list:
+    return get_ordered_messages(user, companion)
--- backend/app/users/views.py
+++ backend/app/users/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from allauth.account.views import LoginView, SignupView
 from .forms import *
 from nfts.models import NFTs
+from .usecases import get_chat_messages
 
 
 class CustomLoginView(LoginView):
@@ -40,30 +41,28 @@ def profile_page(request, pk):
 def messages(request, pk: int):
     target_user = Profile.objects.get(user__pk=pk)
 
-    # TODO:
-    # companion = user-object
-    # messages = [
+    chat_messages = get_chat_messages(request.user, target_user.user)
+    # [
     #     {
-    #         "day": 20, "month": 5, "year": 2021,
-    #         "messages": {
-    #             "companion": {"time": "12:00", "content": ["Hello", "Hi", "How are you?"]}},
-    #         "myself": {"time": "12:05", "content": ["I'm fine", "How about you?"]},
-    #     },
-    #     {
-    #         "day": 21, "month": 5, "year": 2021,
-    #         "messages": {
-    #             "companion": {"time": "12:00", "content": ["Hello", "Hi", "How are you?"]}},
-    #             "myself": {"time": "12:05", "content": ["I'm fine", "How about you?"]},
+    #         'day': 15, 'month': 8, 'year': 2024,
+    #         'message': {
+    #             'time': '05:40',
+    #             'content': '...',
+    #             'sender': 'companion'
+    #         }
     #     },
     # ]
+
+
     formatted_last_login = target_user.user.last_login.strftime("%d %b %Y, %H:%M:%S")
 
     context = {
         "target_user_profile": target_user,
         "target_user_is_active": False,
         "target_user_last_login": formatted_last_login,
-        "friends": Profile.objects.all()
-    }
+        "friends": Profile.objects.all(),
+        "chat_messages": chat_messages
+    }    
     return render(request, "messages.html", context)
 
 
