cc9e553374d0403ad72deef621113c344877b7d7
Alisher <alisherxujanov163@gmail.com>
2024-08-31 10:42:20 +0500

    middleware for html handling

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +11,22 @@ export default defineConfig({
     // Increase the limit as needed
     chunkSizeWarningLimit: 1000, 
   },
+  server: {
+    middlewareMode: 'html',
+    configureServer: ({ middlewares }) => {
+      middlewares.use((req, res, next) => {
+        if (req.url.startsWith('/api')) {
+          return next()
+        }
+        const filePath = resolve(__dirname, 'dist', 'index.html')
+        fs.readFile(filePath, (err, data) => {
+          if (err) {
+            return next(err)
+          }
+          res.setHeader('Content-Type', 'text/html')
+          res.end(data)
+        })
+      })
+    }
+  }
 })
