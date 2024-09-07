03e12cc5e55efa77538f9054f78028078557d52a
Alisher <alisherxujanov163@gmail.com>
2024-09-07 19:30:09 +0500

    installed selenium and downloaded chromedriver

---
+++ backend/app/tests/test_landing_page.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+PATH = "C:\\Users\\User\\Documents\\All-in-one\\Fullstack13\\backend\\app\\tests\\chromedriver.exe"
+
+class Fullstack(unittest.TestCase):
+    
+    def setUp(self):
+        ...
+
+    def tearDown(self):
+        ...
