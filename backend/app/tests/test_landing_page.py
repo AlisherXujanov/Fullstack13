1619fc68a449b6d8ed15c5c9a9d9cc1ebacb528d
Alisher <alisherxujanov163@gmail.com>
2024-09-10 17:14:13 +0500

    tests starting point

--- backend/app/tests/test_landing_page.py
+++ backend/app/tests/test_landing_page.py
@@ -4,9 +4,18 @@ import unittest
 PATH = "C:\\Users\\User\\Documents\\All-in-one\\Fullstack13\\backend\\app\\tests\\chromedriver.exe"
 
 class Fullstack(unittest.TestCase):
-    
+
     def setUp(self):
         ...
 
     def tearDown(self):
         ...
+
+    def test_landing_page(self):
+        ...
+
+
+# 1. git pull
+# 2. pipenv install
+# 3. pipenv shell
+# 4. Write your own test
