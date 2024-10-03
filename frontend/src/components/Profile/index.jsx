9ae3b8889efb1aa16e6d313fccbb32ba28679a1d
Alisher <alisherxujanov163@gmail.com>
2024-10-03 18:44:12 +0500

    profile btns

--- frontend/src/components/Profile/index.jsx
+++ frontend/src/components/Profile/index.jsx
@@ -1,12 +1,15 @@
 import { getUserProfile, fetchLogout } from '../../store/helpers'
 import { useNavigate } from 'react-router-dom'
 import { useState, useEffect, useContext } from 'react';
-import { globalContext } from "../../store"
+import { globalContext, BASE_URL } from "../../store"
 import './style.scss'
+
+
 function Profile() {
     const navigate = useNavigate()
     const state = useContext(globalContext)
     const [profile, setProfile] = useState()
+
     useEffect(() => {
         async function fetchGetUserProfile() {
             const userData = await getUserProfile()
@@ -14,6 +17,7 @@ function Profile() {
         }
         fetchGetUserProfile()
     }, [])
+
     async function logout() {
         state.dispatch({ type: "LOGOUT" })
         await fetchLogout()
@@ -24,7 +28,7 @@ function Profile() {
         <>
             <div className="profile-wrapper">
                 <div className="left">
-                    <img src={"http://127.0.0.1:8000/" + profile?.image} alt="" />
+                    <img src={BASE_URL + profile?.image} alt="" />
                 </div>
                 <div className="middle">
                     <h1>{profile?.user.username}</h1>
@@ -45,7 +49,20 @@ function Profile() {
 
                 </div>
                 <div className="right">
-                    <button className='warning-btn' onClick={logout}>Выйти</button>
+                    <button className='warning-btn update'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FF6600" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                            <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                        </svg>
+                        Update
+                    </button>
+                    <button className='warning-btn logout' onClick={logout}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FF6600" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
+                            <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
+                        </svg>
+                        Выйти
+                    </button>
                 </div>
             </div>
         </>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,8 +2,8 @@ import { createContext } from "react";
 import { globalReducer } from "./helpers";
 
 const BASE_URL = "http://127.0.0.1:8000/"
-const BASE_URL_APIS = BASE_URL + "/apis/"
-const BASE_AUTH_URL = BASE_URL + "/auth/"
+const BASE_URL_APIS = BASE_URL + "apis/"
+const BASE_AUTH_URL = BASE_URL + "auth/"
 
 
 const globalContext = createContext()
