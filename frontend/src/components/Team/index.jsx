6d78b3d4ee4f9b7c82509e4d05c4e8b5a45e1f89
Islom <abdugafarovislom@gmail.com>
2024-10-10 18:33:51 +0500

    team-picture

--- frontend/src/components/Team/index.jsx
+++ frontend/src/components/Team/index.jsx
@@ -24,20 +24,23 @@ function Team(props) {
 
       <div className="text-wrapper">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam is
-          cupiditate tempora deserunt numquam aliquam aspernatur impedit? Cumque
-          laudantium quae, distinctio asperiores, beatae magnam quos repudiandae
-          reiciendis, quas nulla dolor deserunt.lorem laudantium quae,reiciendis,
-          distinctio asperiores, beatae magnam quos repudiandae reiciendis, quas
-          nulla dolor deserunt.lorem
+        Наша команда
+          Мы — это группа профессионалов, объединённых общей целью: создавать качественные, инновационные и эффективные решения для наших клиентов. В команде собраны специалисты разных областей — от разработчиков и дизайнеров до маркетологов и проектных менеджеров. Каждый из нас вносит свой уникальный вклад, благодаря чему мы достигаем выдающихся результатов в наших проектах.
+
+          Почему мы?
+          Опыт и экспертиза: Каждый член нашей команды имеет глубокие знания в своей области, что позволяет нам справляться с задачами любой сложности.
+          Креативный подход: Мы не просто следуем трендам, мы их создаём, предлагая креативные и нестандартные решения для бизнеса.
+          Клиентоориентированность: Мы всегда учитываем потребности наших клиентов и стремимся превзойти их ожидания.
+          Работа в команде: Слаженная работа и обмен опытом внутри команды позволяют нам быстро адаптироваться к изменениям и оставаться на пике эффективности.
+          Мы гордимся тем, что каждый проект — это результат тесного сотрудничества между всеми членами команды, где каждый голос важен, а вклад каждого ценен.
         </p>
       </div>
 
 
       <div className={props.grayscale ? "all-members-wrapper grayscale" : "all-members-wrapper"}>
-        <Member name="Эрджан Мусин" title="CFO" image={firstImage} />
-        <Member name="Олжас Укенов" title="CEO" image={secondImage} />
-        <Member name="Эрджан Мусин" title="CFO" image={firstImage} />
+        <a href="https://github.com/AlisherXujanov"><Member name="Алишер Худжанов" title="CFO" image={firstImage} /></a>
+        <a href="https://github.com/azim0nt"><Member name="Azim0nt" title="CEO" image={secondImage} /></a>  
+        <a href="https://github.com/Islom1214"><Member name="Эрджан Мусин" title="CFO" image={firstImage} /></a>
         <Member name="Олжас Укенов" title="CEO" image={secondImage} />
         <Member name="Эрджан Мусин" title="CFO" image={firstImage} />
         <Member name="Олжас Укенов" title="CEO" image={secondImage} />
