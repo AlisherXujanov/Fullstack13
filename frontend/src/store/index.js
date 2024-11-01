a4b676b8218b82f7a09280b3b6822f7afb888736
Alisher <alisherxujanov163@gmail.com>
2024-11-01 14:54:34 +0500

    update

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import { globalReducer } from "./helpers";
 
-const BASE_URL = "http://127.0.0.1:8000/"
+const BASE_URL = "https://alisherkhujanov.pythonanywhere.com/"
 const BASE_URL_APIS = BASE_URL + "apis/"
 const BASE_AUTH_URL = BASE_URL + "auth/"
 
